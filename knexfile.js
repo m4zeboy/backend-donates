97360e7982a61e8544c8657423e018410e4709cc
Moisés Silva de Azevedo <moises775azevedo@gmail.com>
2021-07-08 18:00:40 -0300

    Configuring knexfile

---
+++ knexfile.js
@@ -0,0 +1,50 @@
+// Update with your config settings.
+export default {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      user: 'postgres',
+      password: '1594',
+      database: 'donatesdev'
+    },
+    migrations: {
+      directory: './db/migrations',
+      tableName: 'migrations'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
